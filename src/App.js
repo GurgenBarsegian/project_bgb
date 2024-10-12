90321e0a79da79d6400077c673cd97f28fb230e6
Katja <asrounk@gmail.com>
2024-10-12 12:23:15 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import {Routes, Route} from 'react-router-dom'
+import MainPage from './pages/MainPage';
+import AllProductsPage from './pages/AllProductsPage';
+import AllSalesPage from './pages/AllSalesPage';
+import CartPage from './pages/CartPage';
+import NotFoundPage from './pages/NotFoundPage';
+
 
 function App() {
   return (
     <div >
-     hello world
+  <Header/>
+  <Routes>
+  <Route path='/' element={<MainPage/>}/>
+        <Route path='/categories' element={<CategoriesPage/>}/>
+        <Route path='/all_products' element={<AllProductsPage/>}/>
+        <Route path='/sales' element={<AllSalesPage/>}/>
+        <Route path='/cart' element={<CartPage/>}/>
+        <Route path='*' element={<NotFoundPage/>}/>
+  </Routes>
+  <Footer/>
     </div>
   );
 }
