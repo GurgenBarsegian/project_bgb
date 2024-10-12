b1b46625d6cb85b7dc9ada60595e30a1856d6111
vlada <vladislava568cmin569@gmail.com>
2024-10-12 14:59:13 +0200

    branch

--- src/pages/CategoriesPageForMain/index.js
+++ src/pages/CategoriesPageForMain/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import { useEffect } from 'react';
 import { getCategories } from '../../requests/categories';
 import CategoriesContainer from '../../components/CategoriesContainer';
 
