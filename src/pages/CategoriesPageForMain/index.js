9e61c08fecaebc4f40d9d0b203f20b4d9901cbdb
Katja <asrounk@gmail.com>
2024-10-12 15:01:18 +0200

    created branch katja

--- src/pages/CategoriesPageForMain/index.js
+++ src/pages/CategoriesPageForMain/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {useDispatch, useSelector } from 'react-redux';
 import { getCategories } from '../../requests/categories';
 import CategoriesContainer from '../../components/CategoriesContainer';
+import { useEffect } from 'react';
 
 
 export default function CategoriesPageForMain() {
