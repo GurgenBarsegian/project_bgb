75971887057cdfdae07239fbef14ef7fd71b9acb
GurgenBarsegian <barsegiangurgen@gmail.com>
2024-10-21 13:37:25 +0200

    updated cart page

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -4,15 +4,27 @@ import { RxCross2 } from "react-icons/rx";
 import { decrementCountAction, deleteProductFromCartAction, incrementCountAction } from '../../store/reducers/cartReducer';
 import s from './index.module.css';
 
-export default function CartItem({id, image, title, discount_price, price, count}) {
+export default function CartItem({id, image, title, discont_price, price, count}) {
 
     const dispatch = useDispatch();
+    const discontProcent = Math.round(100 - ((discont_price / price) * 100));
+
+  const tempPrice =() =>{
+
+    if(discont_price === null){
+      return <p>{price}$</p>
+    } else {
+      return <p>{discont_price}$ <span>{price}$</span> <span>{discontProcent}%</span></p>
+    }
+  
+  }
 
   return (
     <div className={s.card}>
-        <img src={image} alt={title} />
+        <img src={`http://localhost:3333${image}`} alt={title} />
         <p>{ title }</p>
         <p>{ price }$</p>
+        <p>{tempPrice()}</p>
         <div>
             <div onClick={() => dispatch(decrementCountAction(id))}>
               -
--- src/components/ProductsCard/index.jsx
+++ src/components/ProductsCard/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import s from './index.module.css';
+import { addProductToCartAction } from '../../store/reducers/cartReducer';
+import { useDispatch } from 'react-redux';
  
 export default function ProductsCard({ id, image, title, price, discont_price}) {
+
+  const dispatch = useDispatch();
 
   const discontProcent = Math.round(100 - ((discont_price / price) * 100));
 
@@ -23,7 +27,9 @@ export default function ProductsCard({ id, image, title, price, discont_price}) 
         <p>{ title }</p>
         <p>{tempPrice()}</p>
       </div>
+      <div onClick={() => dispatch(addProductToCartAction({ id, image, title, price }))}>
+        Add to cart
+      </div>
     </div>
-
   )
 }
