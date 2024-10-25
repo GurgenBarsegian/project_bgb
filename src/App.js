f1d3ada2009a80621920913ebae9820b074cbd6a
GurgenBarsegian <barsegiangurgen@gmail.com>
2024-10-25 20:09:41 +0200

    123

--- src/App.js
+++ src/App.js
@@ -16,18 +16,19 @@ import ProductsByCategoriesPage from './pages/ProductsByCategoriesPage';
 function App() {
   return (
     <div >
-  <Header/>
-    <Routes>
-  <Route path='/' element={<MainPage/>}/>
-        <Route path='/categories' element={<CategoriesPage/>}/>
-        <Route path='/all_products' element={<AllProductsPage/>}/>
-        <Route path='/sales' element={<AllSalesPage/>}/>
-        <Route path='/cart' element={<CartPage/>}/>
-        <Route path='*' element={<NotFoundPage/>}/>
-        <Route path='/categories/:id' element={<ProductsByCategoriesPage/>}/>
-       
-  </Routes>
-  <Footer/>
+      <Header/>
+        <Routes>
+      <Route path='/' element={<MainPage/>}/>
+            <Route path='/categories' element={<CategoriesPage/>}/>
+            <Route path='/all_products' element={<AllProductsPage/>}/>
+            <Route path='/sales' element={<AllSalesPage/>}/>
+            <Route path='/cart' element={<CartPage/>}/>
+            <Route path='*' element={<NotFoundPage/>}/>
+            <Route path='/categories/:id' element={<ProductsByCategoriesPage/>}/>
+          
+      </Routes>
+      <Footer/>
+
     </div>
   );
 }
