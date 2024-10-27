e1e27c9d53f36b068cb5652395ba03c8cdb4a3a6
Katja <asrounk@gmail.com>
2024-10-27 19:31:05 +0100

    smthg

--- src/components/CategoryCard/index.jsx
+++ src/components/CategoryCard/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import s from './index.module.css'
+import { Link } from 'react-router-dom'
+
 
 export default function CategoryCard({title, image}) {
   return (
+ 
     <div className={s.category_card}>
+      
+    <span className={s.span}>
+      <h2>Categories</h2>
+    </span>
+        <div className={s.class_div}>
+        <Link to='/categories'>All categories</Link>
+        </div>
 
       <img src={`http://localhost:3333${image}`} alt={title} />
       <p>{title}</p>
