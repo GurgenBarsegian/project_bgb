c9c746d9f80188f802a3ba12b94f9484d7aee3ea
Kornilcev <kornilcevkirill@gmail.com>
2024-10-27 20:10:17 +0100

    main upgrade

--- src/components/SalesForBody/index.jsx
+++ src/components/SalesForBody/index.jsx
@@ -3,7 +3,6 @@ import ProductsContainer from '../ProductsContainer';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../../requests/getProducts';
 import s from './index.module.css';
-import b from './index.module.css';
 import { Link } from 'react-router-dom';
 
 
@@ -36,15 +35,15 @@ console.log(randomItems);
     
   return (
     <div className={s.body_sales}>
-      <div>
+      <div >
         <span>
           <h2>Sale</h2>
         </span>
             <div className={s.class_div}>
             <Link to='/sales'>All sales</Link>
             </div>
-      </div>
-      <ProductsContainer products = {randomItems}/>
+      </div>  
+          <ProductsContainer products = {randomItems}/>
     </div>
   )
 }
