cf35787bb080568e00f336c2de9ee928fef6ae72
GurgenBarsegian <barsegiangurgen@gmail.com>
2024-11-07 20:28:07 +0100

    123

--- src/App.js
+++ src/App.js
@@ -9,17 +9,6 @@ import CartPage from "./pages/CartPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import CategoriesPage from "./pages/CategoriesPage";
 import ProductsByCategoriesPage from "./pages/ProductsByCategoriesPage";
-import { useState } from "react";
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import { Routes, Route } from 'react-router-dom'
-import MainPage from './pages/MainPage';
-import AllProductsPage from './pages/AllProductsPage';
-import AllSalesPage from './pages/AllSalesPage';
-import CartPage from './pages/CartPage';
-import NotFoundPage from './pages/NotFoundPage';
-import CategoriesPage from './pages/CategoriesPage';
 import SingleProductPage from './pages/SingleProductPage';
 
 
@@ -36,6 +25,7 @@ function App() {
           <Route path="/all_products" element={<AllProductsPage />} />
           <Route path="/sales" element={<AllSalesPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path='/products/:product_id' element={<SingleProductPage />} />
           <Route path="*" element={<NotFoundPage />} />
           <Route
             path="/categories/:id"
